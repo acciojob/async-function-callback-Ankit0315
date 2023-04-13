3293a02f36d83f6e71889853738df76db7aeb025
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 18:13:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function click() {
-	const url = "https://jsonplaceholder.typicode.com/posts/1";
-fetch("https://jsonplaceholder.typicode.com/posts/1").then((resp))
-	return resp.json();
-}.then((data))=>{
-	console.log(data);
+
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+async function getData(callback) {
+  const response = await fetch(Url);
+  const data = await response.json();
+  callback(data.title);
 }
 
+document.getElementById("btn").addEventListener("click", async function() {
+  getData((title) => {
+    document.getElementById("output").innerHTML = title;
+  });
+});
 
 //your JS code here. If required.
