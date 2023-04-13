3bcd345c581e6b1998f40dbc136dd26711ba9bdd
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 18:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+function click() {
+	const url = "https://jsonplaceholder.typicode.com/posts/1";
+fetch("https://jsonplaceholder.typicode.com/posts/1").then((resp))
+	return resp.json();
+}.then((data))=>{
+	console.log(data);
+}
+
 
 //your JS code here. If required.
